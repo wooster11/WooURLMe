142505574b6bd21f7103f610d0d87bc14a5c0414
Derek Woo <derek.c.woo@gmail.com>
2014-08-11 10:36:11 -0700

    Add in validation for URLs entered

--- WooURLMe/Scripts/wooUrl.js
+++ WooURLMe/Scripts/wooUrl.js
@@ -3,23 +3,32 @@
     self.wooUrl = ko.observable('');
     self.originalLink = ko.observable('');
     self.isSubmitEnabled = ko.observable(true);
+    self.isInvalidURL = ko.observable(false);
 
     self.submitURL = function (formElement) {
+        var longUrlValue = formElement['LongURLValue'].value;
+        if (!self.isUrl(longUrlValue)) {
+            self.isInvalidURL(true);
+            $('#LongURLValue').select();
+            return;
+        }            
+
         $.ajax({
             type: 'POST',
             url: '../api/WooURL',
             contentType: 'application/json',
-            data: ko.toJSON({ longUrl: formElement['LongURLValue'].value }),
+            data: ko.toJSON({ longUrl: longUrlValue }),
             beforeSend: function () {
                 self.wooUrl('');
                 self.originalLink('');
                 self.isSubmitEnabled(false);
+                self.isInvalidURL(false);
             }
         })
         .done(function (data) {
             if (data != null) {
                 self.wooUrl(data);
-                self.originalLink(formElement['LongURLValue'].value);
+                self.originalLink(longUrlValue);
                 $('[data-toggle="tooltip"]').tooltip(); //Enable tooltips
             }
         })
@@ -27,6 +36,10 @@
             formElement['LongURLValue'].value = '';
             self.isSubmitEnabled(true);
         });
+    }
+
+    self.isUrl = function (urlValue) {
+        return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(urlValue);
     }
 }
 
