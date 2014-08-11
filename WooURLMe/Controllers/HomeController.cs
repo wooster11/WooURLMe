22121a6adfe32998d2ffa7c29ae611d1d19d4160
Derek Woo <derek.c.woo@gmail.com>
2014-08-11 15:36:27 -0700

    Modify routes to support URL Redirection for shortened URLs. Redirect users when a shortened URL is navigated to.

--- WooURLMe/Controllers/HomeController.cs
+++ WooURLMe/Controllers/HomeController.cs
@@ -1,17 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WooURLMe.Core;
 using WooURLMe.DAL;
+using WooURLMe.Models;
 
 namespace WooURLMe.Controllers
 {
 	public class HomeController : Controller
 	{
+		private WooURLContext db = new WooURLContext();
+
 		public ActionResult Index()
 		{
-			return View();
+			var homeModel = new Home() { IsBadRedirect = false, URLAttempted = HttpContext.Request.Url.AbsoluteUri };
+			try
+			{
+				//Get the path of the URL
+				var abPath = HttpContext.Request.Url.AbsolutePath;
+
+				if (abPath.StartsWith("/"))
+					abPath = abPath.Substring(1);
+
+				if (String.IsNullOrWhiteSpace(abPath))
+					return View(homeModel);
+
+				var wooUrlID = BaseWoo.BaseWooDecode(abPath);
+				
+				var wooUrl = db.WooUrls.FirstOrDefault(w => w.WooURLID == wooUrlID);
+
+				if (wooUrl == null)
+				{
+					homeModel.IsBadRedirect	= true;
+					return View(homeModel);
+				}
+				else
+					return Redirect(wooUrl.LongURL);
+			}
+			catch (Exception e)
+			{
+				Trace.WriteLine(String.Format("Redirect Failure: {0} | {1}", e.Message, HttpContext.Request.Url.AbsoluteUri), "Error");
+				homeModel.IsBadRedirect = true;
+				return View(homeModel);
+			}			
 		}
 	}
 }
---
+++ WooURLMe/Models/Home.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WooURLMe.Models
+{
+	public class Home
+	{
+		public bool IsBadRedirect { get; set; }
+		public string URLAttempted { get; set; }
+	}
+}
